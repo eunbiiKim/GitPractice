cab52dfae5d01635619f09e24ffe4fdf41905554
[파인원]김은비 <eunbikim0807@gmail.com>
2022-10-12 13:26:15 +0900

    Initial Commit

---
+++ GitPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitPractice
+//
+//  Created by pineone on 2022/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
